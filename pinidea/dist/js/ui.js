507c36b20a028f578c1d851eea23db48771a16e4
ben7th <ben7th@sina.com>
2015-04-03 19:28:46 +0800

    完成分步表单样式

--- pinidea/dist/js/ui.js
+++ pinidea/dist/js/ui.js
@@ -32,6 +32,30 @@
     return jQuery(this).toggleClass('active');
   });
 
+  jQuery(document).delegate('.page-new-topic .form a.next', 'click', function() {
+    var $current_part, $to_part, current, to;
+    $current_part = jQuery(this).closest('.part');
+    $to_part = $current_part.next('.part');
+    current = $current_part.data('step');
+    to = $to_part.data('step');
+    $current_part.fadeOut(200);
+    $to_part.fadeIn(200);
+    jQuery(".topbar .steps .step[data-step=" + current + "]").removeClass('active').addClass('done');
+    return jQuery(".topbar .steps .step[data-step=" + to + "]").addClass('active');
+  });
+
+  jQuery(document).delegate('.page-new-topic .form a.prev', 'click', function() {
+    var $current_part, $to_part, current, to;
+    $current_part = jQuery(this).closest('.part');
+    $to_part = $current_part.prev('.part');
+    current = $current_part.data('step');
+    to = $to_part.data('step');
+    $current_part.fadeOut(200);
+    $to_part.fadeIn(200);
+    jQuery(".topbar .steps .step[data-step=" + current + "]").removeClass('active');
+    return jQuery(".topbar .steps .step[data-step=" + to + "]").removeClass('done').addClass('active');
+  });
+
 }).call(this);
 
 //# sourceMappingURL=../maps/ui.js.map
